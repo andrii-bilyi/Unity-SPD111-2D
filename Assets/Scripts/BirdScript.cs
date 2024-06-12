5743700b0630aef8811f151437e2796cdc4272b6
andrii-bilyi <100190822+andrii-bilyi@users.noreply.github.com>
2024-06-12 13:20:27 +0300

    Initial commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody; //Посилання на компонент того ж, на якому скрипт
+
+    void Start()
+    {
+        Debug.Log("BirdScript Start");
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0, 500));
+        }
+    }
+}
